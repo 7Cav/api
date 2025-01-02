4830ff49887ccf16e025c5e2bfb66a48ea954807
SyniRon <66834451+SyniRon@users.noreply.github.com>
2025-01-01 23:08:46 -0500

    added a magefile for windows dev mostly matches makefile but not entirely makefile still default for building container

---
+++ magefile.go
@@ -0,0 +1,59 @@
+//go:build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// Generate protobufs and statik assets
+func Generate() error {
+	fmt.Println("Generating protobufs and static assets...")
+	if err := run("buf", "generate"); err != nil {
+		return err
+	}
+	return run("statik", "-m", "-f", "-src", "third_party/OpenAPI/")
+}
+
+// Lint and check for breaking changes
+func Lint() error {
+	fmt.Println("Running lint and breaking changes check...")
+	if err := run("buf", "lint"); err != nil {
+		return err
+	}
+	return run("buf", "breaking", "--against", "https://github.com/7cav/api.git#branch=develop")
+}
+
+// Install dependencies and tools
+func Install() error {
+	fmt.Println("Installing dependencies and tools...")
+	if err := run("go", "install",
+		"google.golang.org/protobuf/cmd/protoc-gen-go",
+		"google.golang.org/grpc/cmd/protoc-gen-go-grpc",
+		"github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway",
+		"github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2",
+		"github.com/rakyll/statik",
+	); err != nil {
+		return err
+	}
+	return run("go", "get", "-u",
+		"github.com/bufbuild/buf/cmd/buf",
+		"github.com/square/certstrap",
+		"github.com/spf13/cobra",
+	)
+}
+
+// Helper function to run commands
+func run(name string, args ...string) error {
+	fmt.Printf("Running command: %s %v\n", name, args)
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to run %s %v: %w", name, args, err)
+	}
+	return nil
+}
